90567b6a5fa0645697a96a2a6176399d5927bdaa
Antony Repin <antony@starofservice.com>
2016-01-06 15:22:45 +0300

    modified:   .gitignore 	new file:   resources/views/layouts/app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Compare files</title>
+
+        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    </head>
+
+    <body>
+        <!-- <div class="container">
+            <nav class="navbar navbar-default">
+                
+            </nav>
+        </div> -->
+
+        @yield('content')
+    </body>
+</html>
+
